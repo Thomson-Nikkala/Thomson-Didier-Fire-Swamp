d25e1c1b443274de72083b4681e9e3327f13109d
Didier Jourdain and Nikkala Thomson <maraamu@byui.edu>
2016-11-02 13:34:42 -1000

    Modified testCalcFireSpurtAnswer() and calcFireSpurtAnswer()

--- FireSwamp/src/byui/cit260/fireSwamp/controller/DangerControl.java
+++ FireSwamp/src/byui/cit260/fireSwamp/controller/DangerControl.java
@@ -1,7 +1,7 @@
-/***************************************************
+/** *************************************************
  * DangerControl Class                             *
  *                                                 *
- ***************************************************/
+ ************************************************** */
 package byui.cit260.fireSwamp.controller;
 
 /**
@@ -9,9 +9,9 @@ package byui.cit260.fireSwamp.controller;
  * @authors Didier Jourdain and Nikkala Thomson
  */
 public class DangerControl {
-    
-    public double calcLightningSandAnswer (double diameter) {
-        
+
+    public double calcLightningSandAnswer(double diameter) {
+
         if (diameter > 0.0 && diameter <= 30) {
             double answer = diameter * Math.PI / 2.0;
             return answer;
@@ -19,65 +19,35 @@ public class DangerControl {
             return -1;
         }
     }
-    
-    public double calcFireSpurtAnswer(double length, double width, double height){
-        
+
+    public double calcFireSpurtAnswer(double length, double width, double height) {
+
         double answer = 0.0;
-        
+
         // length checking
-        if (length <= 0.0) {
-            answer = answer -1.0;
+        if (length < 3.0 && length > 99.0) {
+            answer = answer - 1.0;
         }
-        else {
-            if (length < 3.0) {
-                answer = answer - 2.0;
-            }
-            else {
-                if (length > 99.0) {
-                    answer = answer - 4.0;
-                }
-            }
-        }
-        
+
         // width checking
-        if (width <= 0.0) {
-            answer = answer -10.0;
+        if (width < 3.0 && width > 99.0) {
+            answer = answer - 10.0;
         }
-        else {
-            if (width < 3.0) {
-                answer = answer - 20.0;
-            }
-            else {
-                if (width > 99.0) {
-                    answer = answer - 40.0;
-                }
-            }
-        }
-        
+
         // height checking
-        if (height <= 0.0) {
+        if (height < 3.0 && height > 99.0) {
             answer = answer - 100.0;
         }
-        else {
-            if (height < 3.0) {
-                answer = answer - 200.0;
-            }
-            else {
-                if (height > 99.0) {
-                    answer = answer - 400.0;
-                }
-            }
-        }
-        
+
         if (answer < 0.0) {
             return answer;
         } else {
             return (length * width * height) / 3;
         }
     }
-    
-     public double calcRousAnswer (double length) {
-         
+
+    public double calcRousAnswer(double length) {
+
         if (length >= 1.0 && length <= 15) {
             double answer = length / 2.0 + 0.5;
             return answer;
@@ -85,5 +55,5 @@ public class DangerControl {
             return -1;
         }
     }
-    
+
 }
--- FireSwamp/test/byui/cit260/fireSwamp/controller/DangerControlTest.java
+++ FireSwamp/test/byui/cit260/fireSwamp/controller/DangerControlTest.java
@@ -13,11 +13,9 @@ import static org.junit.Assert.*;
  * @author Nikkala
  */
 public class DangerControlTest {
-    
+
     public DangerControlTest() {
     }
-    
-   
 
     /**
      * Test of calcLightningSandAnswer method, of class DangerControl.
@@ -30,31 +28,31 @@ public class DangerControlTest {
         double expResult = 3.14;
         double result = instance.calcLightningSandAnswer(diameter);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcLightningSandAnswer diameter = -2.0");
         diameter = -2.0;
         expResult = -1.0;
         result = instance.calcLightningSandAnswer(diameter);
         assertEquals(expResult, result, 0.01);
-       
+
         System.out.println("calcLightningSandAnswer diameter = 0");
         diameter = 0.0;
         expResult = -1.0;
         result = instance.calcLightningSandAnswer(diameter);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcLightningSandAnswer diameter = 14");
         diameter = 14.0;
         expResult = 21.99;
         result = instance.calcLightningSandAnswer(diameter);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcLightningSandAnswer diameter = 0.1");
         diameter = 0.1;
         expResult = 0.16;
         result = instance.calcLightningSandAnswer(diameter);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcLightningSandAnswer diameter = 30");
         diameter = 30.0;
         expResult = 47.12;
@@ -75,15 +73,15 @@ public class DangerControlTest {
         double expResult = -111.0;
         double result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L<3, W=0, H= 0");
         length = 2.9;
         width = 0.0;
         height = 0.0;
-        expResult = -112.0;
+        expResult = -111.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L=15, W=20, H=50");
         length = 15.0;
         width = 20.0;
@@ -91,7 +89,7 @@ public class DangerControlTest {
         expResult = 5000.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L<=0, W=20, H=50");
         length = -15.0;
         width = 20.0;
@@ -99,7 +97,7 @@ public class DangerControlTest {
         expResult = -1.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L=15, W<=0, H=50");
         length = 15.0;
         width = -20.0;
@@ -107,7 +105,7 @@ public class DangerControlTest {
         expResult = -10.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L=15, W=20, H<=0");
         length = 15.0;
         width = 20.0;
@@ -115,7 +113,7 @@ public class DangerControlTest {
         expResult = -100.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L=0, W=20, H=50");
         length = 0.0;
         width = 20.0;
@@ -123,7 +121,7 @@ public class DangerControlTest {
         expResult = -1.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L=15, W=0, H=50");
         length = 15.0;
         width = 0.0;
@@ -131,7 +129,7 @@ public class DangerControlTest {
         expResult = -10.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L=15, W=20, H=0");
         length = 15.0;
         width = 20.0;
@@ -139,20 +137,20 @@ public class DangerControlTest {
         expResult = -100.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L<3, W<3, H<3");
         length = 2.9;
         width = 2.9;
         height = 2.9;
-        expResult = -222.0;
+        expResult = -111.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
-        
+
         System.out.println("calcFireSpurtAnswer with L>99, W>99, H>99");
         length = 99.1;
         width = 99.1;
         height = 99.1;
-        expResult = -444.0;
+        expResult = -111.0;
         result = instance.calcFireSpurtAnswer(length, width, height);
         assertEquals(expResult, result, 0.0);
     }
@@ -168,36 +166,36 @@ public class DangerControlTest {
         double expResult = 2.0;
         double result = instance.calcRousAnswer(length);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcRousAnswer length -2");
         length = -2.0;
         expResult = -1;
         result = instance.calcRousAnswer(length);
         assertEquals(expResult, result, 0.01);
-       
+
         System.out.println("calcRousAnswer length 0");
         length = 0.0;
         expResult = -1;
         result = instance.calcRousAnswer(length);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcRousAnswer length 5");
         length = 5.0;
         expResult = 3.0;
         result = instance.calcRousAnswer(length);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcRousAnswer length 1");
         length = 1.0;
         expResult = 1.0;
         result = instance.calcRousAnswer(length);
         assertEquals(expResult, result, 0.01);
-        
+
         System.out.println("calcRousAnswer length 15");
         length = 15.0;
         expResult = 8.0;
         result = instance.calcRousAnswer(length);
         assertEquals(expResult, result, 0.01);
     }
-    
+
 }
