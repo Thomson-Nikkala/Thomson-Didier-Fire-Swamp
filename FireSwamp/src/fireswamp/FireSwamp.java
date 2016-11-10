1d51a80c31363a4aa087c1010bd4a0a07fbde5fa
Nikkala <Nikkala@Leia>
2016-11-10 00:23:28 -0500

    Week 9 team assignment complete

--- FireSwamp/src/fireswamp/FireSwamp.java
+++ FireSwamp/src/fireswamp/FireSwamp.java
@@ -40,7 +40,7 @@ public class FireSwamp {
     public static void main(String[] args) {
 
         StartProgramView startProgramView = new StartProgramView();
-        startProgramView.displayStartProgramView();
+        startProgramView.display();
     }
 
 }
