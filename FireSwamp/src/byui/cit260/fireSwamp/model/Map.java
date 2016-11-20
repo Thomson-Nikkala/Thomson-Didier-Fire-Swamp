831f84ecb184205b2420e3f5e843b4464500d758
Didier Jourdain <maraamu@byui.edu>
2016-11-19 20:19:29 -1000

    added mapStatistics
    Couldn't push so I edited the Map class directly on github

--- FireSwamp/src/byui/cit260/fireSwamp/model/Map.java
+++ FireSwamp/src/byui/cit260/fireSwamp/model/Map.java
@@ -57,6 +57,83 @@ public class Map implements Serializable{
             }
         }
     }
+    
+    
+    public void mapStatistics() {
+        
+        int difficultyLevel;
+        int[] totalSafeRow = new int[ROWS];
+        int[] totalDangersRow = new int[ROWS];
+        int[] totalFlameSpurtRow = new int[ROWS];
+        int[] totalLightningSandRow = new int[ROWS];
+        int[] totalnumROUSRow = new int[ROWS];
+        int[] totalSafeCol = new int[COLUMNS];
+        int[] totalDangersCol = new int[COLUMNS];
+        int[] totalFlameSpurtCol = new int[COLUMNS];
+        int[] totalLightningSandCol = new int[COLUMNS];
+        int[] totalnumROUSCol = new int[COLUMNS];
+        LocationType dangerFound;
+        
+        for (int col = 0; col < COLUMNS; col++) {
+            totalSafeCol[col] = 0;
+            totalDangersCol[col] = 0;
+            totalFlameSpurtCol[col] = 0;
+            totalLightningSandCol[col] = 0;
+            totalnumROUSCol[col] = 0;
+        }
+        for (int row = 0; row < ROWS; row++) {
+            totalSafeRow[row] = 0;
+            totalDangersRow[row] = 0;
+            totalFlameSpurtRow[row] = 0;
+            totalLightningSandRow[row] = 0;
+            totalnumROUSRow[row] = 0;
+        }
+        
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
+                dangerFound = matrix[row][col].getLocationType();
+                switch (dangerFound) {
+                    case NONE:
+                        totalSafeRow[row]++;
+                        totalSafeCol[col]++;
+                        break;
+                    case FLAMESPURT:
+                        totalFlameSpurtRow[row]++;
+                        totalFlameSpurtCol[col]++;
+                        break;
+                    case LIGHTNINGSAND:
+                        totalLightningSandRow[row]++;
+                        totalLightningSandCol[col]++;                        
+                        break;
+                    case ROUS:
+                        totalnumROUSRow[row]++;
+                        totalnumROUSCol[col]++;
+                        break;
+                }
+            }
+        }
+        
+        
+        int tally = 0;
+        
+        for (int col = 0; col < COLUMNS; col++){
+            tally += totalSafeCol[col];
+        }
+        
+        int GrandTotalDangers = COLUMNS * ROWS - tally;
+        difficultyLevel = (GrandTotalDangers * 100) / (COLUMNS * ROWS);
+        
+        System.out.println("\nThe difficulty level for this game is evaluated at: "
+                            + difficultyLevel + "%");
+        if (difficultyLevel < 30)
+            System.out.println("\nPiece of cake");
+        else if (difficultyLevel < 50)
+            System.out.println("\nYou have your chance");
+        else if (difficultyLevel < 70)
+            System.out.println("\nI hope you're good in math!");
+        else
+            System.out.println("\nYou better be good in math, or bring someone who is.");
+    }
 
     //getters and setters
 
